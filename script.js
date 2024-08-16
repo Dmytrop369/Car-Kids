97cc7721ece5970ef7ec45e98092e651ca5e23ed
dmitro <dmitrop369@.com>
2024-08-16 10:09:26 +0300

    update

--- script.js
+++ script.js
@@ -5,3 +5,10 @@ burger.addEventListener("click", () => {
     burger.classList.toggle("active")
     nav.classList.toggle("active")
 })
+
+nav.addEventListener("click", (event) => {
+    if(event.target.closest("a") || event.target.closest("button")){
+        nav.classList.remove("active")
+        burger.classList.remove("active")
+    }
+})
