08ab4bd8885eeec0a418e83c5ffc468a81df9e00
dmitro <dmitrop369@.com>
2024-08-19 14:42:22 +0300

    update form

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 let burger = document.querySelector(".header__burger")
 let nav = document.querySelector(".header__navigation")
 let openModalBtns = document.querySelectorAll("#open-modal")
-let modal = document.querySelector("modal")
-let modalCloseBtn = document.querySelector("close")
-let forms = document.querySelectorAll("forms")
+let modal = document.querySelector(".modal")
+let modalCloseBtn = document.querySelector(".close")
+let forms = document.querySelectorAll("form")
+let tokenBot = "6909960058:AAGUdmfND5h-_yPapj4Wo6W8RgPVp1NX5Mg"
+let chat ="-1002147170940"
+let telegramURL = `https://api.telegram.org/bot${tokenBot}/sendMessage`
 
 burger.addEventListener("click", () => {
     burger.classList.toggle("active")
@@ -30,7 +33,20 @@ forms.forEach(form => form.addEventListener("submit", (event) => {
 
     let formData = new FormData(form); 
 
-    let message = `Заявка з сайту\n\nКористувач: ${formDate.get("user") ? formData.get("user") : "Без імені"}\nТелефон: ${formData.get("phone")}`
-
-    console.log(message)
+    let message = `Заявка з сайту\n\nКористувач: ${formData.get("user") ? formData.get("user") : "Без імені"}\nТелефон: ${formData.get("phone")}`
+    modal.classList.remove("active")
+    sendData(message, telegramURL).then(data => alert("Ваша заявка надіслана"))
 }))
+
+async function sendData(message, url){
+    let res = await fetch(url, {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            chat_id: chat,
+            text: message,
+            parse_mode: "html"
+        })
+    })
+    return await res.json()
+}
